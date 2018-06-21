248a49ae6be580893958478ca9b9e010dc844f3d
Sarah <sar.himmler@gmail.com>
2018-06-21 15:23:38 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+.bgimg {
+  background-image: url('/background.jpg');
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+.middle {
+	background-color: #e1e1e1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+	<head>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</head>
+	<body>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="page">
+				  <div class="col-sm-6 bgimg">
+				    <img src="meraki-logo.png" alt="Meraki Logo">
+						<div class="bottomleft">
+							<a href="https://www.instagram.com/shopmerakiinteriors/" target="_blank">
+								<i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+							</a>
+							<a href="mailto:info@shopmeraki.us">
+								<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+							</a>
+					  </div>
+				  </div>
+				  <div class="col-sm-6 middle">
+				    <h1>COMING SOON</h1>
+				    <hr>
+						35 N Main St.<br>
+						Belmont NC 28012
+				  </div>
+				</div>
+			</div>
+		</div>
+
+	</body>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"  rel="stylesheet">
+</html>
